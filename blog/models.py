11f92e31d2991a23d6dc89b80940a66d3c0853d2
리프 <vltpcks@gmail.com>
2019-11-03 03:46:07 +0900

    blog function implement

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)  # 제목 표시 글자수 제한 30글자
+    content = models.TextField(null=True)
+    created = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=True)
