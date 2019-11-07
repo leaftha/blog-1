3fc20a6e466bf494a0b845e010fd3e1eec6cc8a0
리프 <vltpcks@gmail.com>
2019-11-07 14:50:15 +0900

    category

--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,34 @@ from markdownx.utils import markdown
 
 
 # Create your models here.
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return '/blog/category/{}/'.format(self.slug)
+
+    class Meta:
+        verbose_name = 'categories'
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)  # 제목 표시 글자수 제한 30글자
     content = MarkdownxField()
     created = models.DateTimeField()
     head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
     author = models.ForeignKey(User, on_delete=True)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 from django.views.generic import ListView, DetailView
 # Create your views here.
 
@@ -10,6 +10,50 @@ class PostList(ListView):
     def get_queryset(self):
         return Post.objects.order_by('-created')
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
+        return context
+
+
+
 
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
+
+        return context
+
+
+class PostListByCategory(ListView):
+
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+
+        if slug == '_none':
+            category =None
+        else:
+            category = Category.objects.get(slug = slug)
+
+        return Post.objects.filter(category=category).order_by('-created')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(type(self), self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
+
+        slug = self.kwargs['slug']
+
+        if slug == '_none':
+            context['category'] = '미분류'
+        else:
+            category = Category.objects.get(slug=slug)
+            context['category'] = category
+
+        # context['title'] ='Blog - {}'.format(category.name)
+        return context
