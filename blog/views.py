f110bf0ae51a2e7b2bd89152de388dd673442cd7
리프 <vltpcks@gmail.com>
2019-11-11 16:16:45 +0900

    search

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post, Category
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 # Create your views here.
 
 
@@ -15,8 +16,20 @@ class PostList(ListView):
         context = super(PostList, self).get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
         context['posts_without_category'] = Post.objects.filter(category=None).count()
+
         return context
 
+
+class PostSearch(PostList):
+    def get_queryset(self):
+        q = self.kwargs['q']
+        object_list = Post.objects.filter(Q(title__contains=q) | Q(content__contains=q))
+        return object_list
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        context['search_info'] = 'Search: "{}"'.format(self.kwargs['q'])
+        return context
 
 
 
